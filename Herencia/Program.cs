26810775d27d166b2b0549c3c141c41f6d360a77
Carlos Encinas <carlos-encinas@hotmail.com>
2019-09-06 08:46:27 -0700

    Agregar archivos de proyecto.

---
+++ Herencia/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Herencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangulo triangulo = new Triangulo();
+            Console.WriteLine(triangulo.toString());
+            Console.ReadLine();
+        }
+    }
+}
