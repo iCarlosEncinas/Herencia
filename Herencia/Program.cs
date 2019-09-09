7f4a35f9d4d76b1208cd7064f6c770970337d322
Carlos Encinas <carlos-encinas@hotmail.com>
2019-09-09 12:35:20 -0700

    Casting

--- Herencia/Program.cs
+++ Herencia/Program.cs
@@ -11,7 +11,36 @@ namespace Herencia
         static void Main(string[] args)
         {
             Triangulo triangulo = new Triangulo();
+            triangulo.Color = "Amarillo";
             Console.WriteLine(triangulo.toString());
+            
+
+            triangulo.Altura = 2.5;
+            triangulo.Base = 4;
+
+            double areaTriangulo = triangulo.CalcularArea();
+
+            Console.WriteLine("Área de triángulo: " + areaTriangulo);
+
+            Circulo circulo = new Circulo(5.4);
+            Console.WriteLine("Area del círculo: " + circulo.CalcularArea().ToString());
+
+            Figura triangulo2 = new Triangulo();
+            ((Triangulo)triangulo2).Base = 2;
+            ((Triangulo)triangulo2).Altura = 3;
+
+            Console.WriteLine("Área de triángulo2: " + triangulo2.CalcularArea().ToString());
+
+            List<Figura> figuras = new List<Figura>();
+            figuras.Add(triangulo);
+            figuras.Add(circulo);
+            figuras.Add(triangulo2);
+
+            foreach(var figura in figuras)
+            {
+                Console.WriteLine("Área: " + figura.CalcularArea().ToString());
+            }
+
             Console.ReadLine();
         }
     }
